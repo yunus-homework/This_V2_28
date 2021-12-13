d12545bae77238fd19556e79d7201e67d182cf1b
Yunus <seytnazarov.yunus@gmail.com>
2021-12-13 13:02:55 +0300

    This V2_28 Updates

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
 "use strict";
+
 const calculator = {
-  read: () => {
-    this.sum = +prompt("First sum", 0);
+  read() {
+    this.sum1 = +prompt("First sum", 0);
     this.sum2 = +prompt("Second sum", 0);
   },
-  sum: () => {
-    return this.sum + this.sum2;
+  sum() {
+    return this.sum1 + this.sum2;
   },
 
-  mul: () => {
-    return this.sum * this.sum2;
+  mul() {
+    return this.sum1 * this.sum2;
   },
 };
 
 calculator.read();
 alert(calculator.sum());
 alert(calculator.mul());
+
+const calculatorSecond = {
+  read: () => {
+    this.sum1 = +prompt("First sum", 0);
+    this.sum2 = +prompt("Second sum", 0);
+  },
+  sum: () => {
+    return this.sum1 + this.sum2;
+  },
+
+  mul: () => {
+    return this.sum1 * this.sum2;
+  },
+};
+
+calculatorSecond.read();
+alert(calculatorSecond.sum());
+alert(calculatorSecond.mul());
 
 // Создайте объект calculator (калькулятор) с тремя методами:
 
